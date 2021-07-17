4c872e5cc98425f9af67ef8261075508b2f812be
Cat <morancathy@gmail.com>
2021-07-17 18:18:11 -0500

    useState

--- src/Sample.js
+++ src/Sample.js
@@ -1,7 +1,20 @@
 import { useState, useEffect, useRef } from 'react';
 
+
 function Sample (props){
-    return <h1>Hello World</h1>;
+
+    const [greet, setGreet] = useState("Hello, I'm using React Hooks");
+
+    const handleClick = () => {
+      setGreet('Hey I just changed state using React Hooks')
+    };
+
+    return (
+      <>
+        <h1>{greet}</h1>
+        <button onClick={handleClick}> Click Me Please</button>
+      </>
+    )
 };
 
 export default Sample;
