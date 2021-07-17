85f524c539a4cdc7678b298329cfece5fa1feba1
Cat <morancathy@gmail.com>
2021-07-17 18:24:04 -0500

    useEffect hook

--- src/Sample.js
+++ src/Sample.js
@@ -5,6 +5,15 @@ function Sample (props){
 
     const [greet, setGreet] = useState("Hello, I'm using React Hooks");
 
+    useEffect(() => {
+      console.log(greet);  //anything in this function will run when the component loads and this effect is retriggered
+
+      return () => {
+        console.log("anything in the returned function is run when the component dismounts")
+      }
+
+    }, [greet]);//any variable passed into the array will be watched and will retrigger the above function if changed
+
     const handleClick = () => {
       setGreet('Hey I just changed state using React Hooks')
     };
